9200ceaabab5fe371f3969f997146447d04cfe6f
Alison <aliatmra@gmail.com>
2025-11-12 14:47:57 -0600

    Create file for all mappings

---
+++ mappings.py
@@ -0,0 +1,33 @@
+name_columns_by_year = {
+    2009: "NAME",
+    2010: "NAME",
+    2011: "NAME",
+    2012: "NAME",
+    2013: "NAME",
+    2014: "NAME",
+}
+
+title_columns_by_year = {
+    2009: "TITLE",
+    2010: "TITLE",
+    2011: "POSITION_TITLE",
+    2012: "POSITION_TITLE",
+    2013: "POSITION_TITLE",
+    2014: "POSITION_TITLE",
+}
+
+dept_columns_by_year = {
+    2011: "TIMESHEET_ORGANIZATION_DESC",
+    2012: "TIMESHEET_ORGANIZATION_DESC",
+    2013: "TIMESHEET_ORGANIZATION_DESC",
+    2014: "TIMESHEET_ORGANIZATION_DESC",
+}
+
+salary_columns_by_year = {
+    2009: "ANNUAL PAY RATE",
+    2010: "ANNUAL PAY RATE",
+    2011: "ANNUAL SALARY",
+    2012: "ANNUAL SALARY",
+    2013: "ANNUAL SALARY",
+    2014: "ANNUAL SALARY",
+}
